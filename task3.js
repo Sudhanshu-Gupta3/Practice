970845dd9e034867789e6177fa34fc1e40498f80
Sudhanshu-Gupta3 <107980309+Sudhanshu-Gupta3@users.noreply.github.com>
2022-07-02 21:57:44 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,11 @@
+const newtitle=document.getElementById('header-title');
+newtitle.style.background="red";
+console.log(newtitle);
+
+
+// newtitle.textContent='Hello';
+
+const additems=document.getElementsByClassName('title')
+console.log(additems);
+additems[0].style.fontWeight='bold';
+additems[0].style.color= 'green';
