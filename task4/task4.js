dc449a33cef9256eb95bcc2de623e6627acd48f4
Sudhanshu-Gupta3 <107980309+Sudhanshu-Gupta3@users.noreply.github.com>
2022-07-03 11:21:23 +0530

    Create task4.js

---
+++ task4/task4.js
@@ -0,0 +1,11 @@
+const list=document.getElementsByClassName('list-group-item');
+list[2].style.background="green";
+const parent =document.getElementsByClassName('list-group');
+parent[0].style.fontWeight='bold';
+
+
+// const items=document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor='green';
+
+// for(let i=0;i<items.length;i++)
+// items[i].style.fontWeight='bold';
